5e61b79d80b54d271c841a1358ee6c3130b495cc
Cesar Trincado <ctrincado@solnet.cl>
2020-10-07 10:29:42 -0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quilljs';
+  title = 'Prueba editor'
+  editor = '';
+  // config= {
+  //   toolbar : [
+  //     ['italic','bold'],
+  //     ['video','image']
+  //   ]
+  // }
+
+  changedEditor(event: EditorChangeContent | EditorChangeSelection){
+    console.log(event);
+    this.editor = event['editor']['root']['innerHTML'] 
+  }
+
 }
